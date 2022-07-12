658591318708049a8a36adc6f1403b770f239f5a
diangenlin <diangenlin@gmail.com>
2022-07-12 15:14:23 -0500

    Add functions to get UMAP plots

--- biosynseq/visualize.py
+++ biosynseq/visualize.py
@@ -187,6 +187,108 @@ def get_tsne(
     return df
 
 
+def run_umap(embed_data: np.ndarray) -> np.ndarray:
+    """Given 2-dimensional sequence embeddings, return the transformed data using UMAP.
+
+    Parameters
+    ----------
+    embed_data : np.ndarray
+        Sequence embeddings to be transformed by UMAP. Must be 2-dimensional.
+
+    Returns
+    -------
+    np.ndarray
+        Transformed embeddings after running UMAP.
+    """
+    # embed_data must be 2D since rapidsai only supports 2D data
+    model = UMAP(random_state=10)
+    data_proj = model.fit_transform(embed_data)
+    return data_proj
+
+
+def plot_umap(
+    data_proj: np.ndarray,
+    paint: np.ndarray,
+    paint_name: str,
+    umap_path: Path,
+    cmap: str = "plasma",
+) -> pd.core.frame.DataFrame:
+    """Plot UMAP visualizations for each sequence metric and
+    save the plots as separate images to the specified directory.
+
+    Parameters
+    ----------
+    data_proj : np.ndarray
+        Transformed embeddings after running UMAP.
+    paint : np.ndarray
+        Dataframe containing information of sequence metrics for each DNA sequence.
+    paint_name : str
+        Name of the sequence metric whose UMAP visualization will be plotted.
+    umap_path : Path
+        Path to save UMAP plots. Must be a directory.
+    cmap : str, optional
+        Colormap to visualize, by default "plasma"
+
+    Returns
+    -------
+    pd.core.frame.DataFrame
+        Dataframe with plotting values.
+
+    Raises
+    ------
+    ValueError
+        If the given umap_path is not a directory.
+    """
+    df = pd.DataFrame(
+        {
+            "z0": data_proj[:, 0],
+            "z1": data_proj[:, 1],
+            paint_name: paint[: data_proj.shape[0]],
+        }
+    )
+    ax = df.plot.scatter(x="z0", y="z1", c=paint_name, colormap=cmap, alpha=0.4)
+    fig = ax.get_figure()
+    fig.show()
+
+    # save each umap plot as a separate png image in the specified directory, umap_path
+    if os.path.isdir(umap_path):
+        fig.savefig(umap_path / (f"{paint_name}_umap.png"), dpi=300)
+    else:
+        raise ValueError(f"{umap_path} is not a directory!")
+    return df
+
+
+def get_umap(
+    embed_data: np.ndarray, paint_df: pd.core.frame.DataFrame, umap_path: Path
+) -> pd.core.frame.DataFrame:
+    """Given 2-dimensional sequence embeddings and sequence metrics dataframe,
+    plot and save UMAP visualizations to specified directory.
+
+    Parameters
+    ----------
+    embed_data : np.ndarray
+        Sequence embeddings to be transformed by UMAP. Must be 2-dimensional.
+    paint_df : pd.core.frame.DataFrame
+        Dataframe containing information of sequence metrics for each DNA sequence.
+    umap_path : Path
+        Path to save UMAP plots. Must be a directory.
+
+    Returns
+    -------
+    pd.core.frame.DataFrame
+        Dataframe with plotting values.
+    """
+    data_umap = run_umap(embed_data=embed_data)
+    for key in paint_df:
+        df = plot_umap(
+            data_proj=data_umap,
+            paint=paint_df[key],
+            paint_name=key,
+            umap_path=umap_path,
+        )
+    return df
+
+
 def parse_args() -> Namespace:
     """Parse command line arguments.
 
@@ -197,7 +299,7 @@ def parse_args() -> Namespace:
     """
     parser = ArgumentParser(description="Generate sequences or embeddings.")
     parser.add_argument(
-        "--mode", type=str, required=True, help="Allowed inputs: get_tsne"
+        "--mode", type=str, required=True, help="Allowed inputs: get_tsne, get_umap"
     )
     parser.add_argument(
         "--embed_path",
@@ -212,7 +314,11 @@ def parse_args() -> Namespace:
         type=Path,
         help="Path to save t-SNE plots. Must lead to a directory, not a file.",
     )
-
+    parser.add_argument(
+        "--umap_path",
+        type=Path,
+        help="Path to save UMAP plots. Must lead to a directory, not a file.",
+    )
     return parser.parse_args()
 
 
@@ -226,10 +332,17 @@ def main() -> None:
         print("4")
         get_tsne(embed_data=embed_avg, paint_df=paint_df, tsne_path=args.tsne_path)
         print("5")
-    else:
+    elif args.mode == "get_umap":
         print("6")
+        embed_avg = get_embed_avg(embed_path=args.embed_path)
+        print("7")
+        paint_df = get_paint_df(fasta_path=args.fasta_path)
+        print("8")
+        get_umap(embed_data=embed_avg, paint_df=paint_df, umap_path=args.umap_path)
+    else:
+        print("9")
         raise ValueError(f"Invalid mode: {args.mode}")
-    print("7")
+    print("10")
 
 
 if __name__ == "__main__":
