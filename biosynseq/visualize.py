881f116609c5ec928dfb20ff6175f0ec0627b3b9
diangenlin <diangenlin@gmail.com>
2022-08-05 09:34:57 -0500

    Fix save_path bug in plot_cluster

--- biosynseq/visualize.py
+++ biosynseq/visualize.py
@@ -161,7 +161,7 @@ def plot_cluster(
     fig.show()
 
     if save_path is not None:
-        fig.savefig(save_path, dpi=300)
+        fig.savefig(str(save_path) + "_" + paint_name, dpi=300)
         print(f"Your plot has been saved to {save_path}")
     return df
 
@@ -287,9 +287,7 @@ def get_cluster(
 
     if get_subplots:
         return plot_cluster_subplots(
-            data_proj=data_proj,
-            paint_df=paint_df,
-            save_path=save_path,
+            data_proj=data_proj, paint_df=paint_df, save_path=save_path,
         )
     return {
         str(key): plot_cluster(
@@ -510,10 +508,7 @@ def parse_args() -> Namespace:
         help="Seed used by the random number generator during embedding initialization and during sampling used by the optimizer to run UMAP.",
     )
     parser.add_argument(
-        "--plot_title",
-        default="",
-        type=str,
-        help="Title for a plot.",
+        "--plot_title", default="", type=str, help="Title for a plot.",
     )
     parser.add_argument(
         "--alignment_type", default="global", type=str, help="global or local"
@@ -615,8 +610,7 @@ def main() -> None:
             alignment_type=args.alignment_type,
         )
         avg_scores_df = metrics.get_avg_scores_df(
-            scores_df=scores_df,
-            alignment_type=args.alignment_type,
+            scores_df=scores_df, alignment_type=args.alignment_type,
         )
         plot_embed_dist_vs_align_score(
             avg_scores_df=avg_scores_df,
